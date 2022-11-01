45cfd43e18422bc45b761756a0795f0e80320bbe
qqKostya <leap74rus@gmail.com>
2022-11-01 18:34:04 +0300

    last version wb

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[name][ext]"
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -39,7 +40,7 @@ module.exports = {
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
           "css-loader",
           {
             loader: "postcss-loader",
@@ -58,6 +59,35 @@ module.exports = {
         generator: {
           filename: 'fonts/[name][ext]'
         }
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
       },
       {
         test: /\.m?js$/i,
